821d2d928102fe00529358dd5913015ce56234f7
Rina Toyoshiba <rtoyoshiba@gmail.com>
2019-07-11 17:35:49 -1000

    chirp chirp

--- js/chirp.js
+++ js/chirp.js
@@ -1,7 +1,13 @@
-function chirp(n){
+function chirp(n) {
   // FIX ME
+  debugger;
+  if (n === 0) {
+    return "";
+  } else {
+    return "chirp " + chirp(n - 1);
+  }
 }
 
-$(document).ready(function(){
+$(document).ready(function() {
   $("#result").html(chirp(3));
 });
